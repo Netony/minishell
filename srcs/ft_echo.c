0897078092f617f0cd767204ed4352609ef9da9b
Seunghyun Lee <seunghy2@c5r6s2.42seoul.kr>
2023-09-11 16:26:35 +0900

    seu.echo

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: seunghy2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 16:03:09 by seunghy2          #+#    #+#             */
-/*   Updated: 2023/09/11 16:24:15 by seunghy2         ###   ########.fr       */
+/*   Updated: 2023/09/11 16:26:21 by seunghy2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_echo(char **command)
 	int	i;
 
 	i = 1;
-	if (!(ft_strcmp(command[i], "-n")))
+	if (command[1] && !(ft_strcmp(command[1], "-n")))
 		i++;
 	while (command[i])
 	{
